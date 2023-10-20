a60a262cff8c4a9f40508399c76e8f40f1dafccc
yzeng <yzeng@altair.com>
2023-10-19 23:34:53 -0400

    update the README

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # IAMR-MULTIPHASE
 
-A parallel, adaptive mesh refinement (AMR) code that solves the variable-density 
-incompressible Navier-Stokes equations. This code can handle the multiphase flow and fluid structure interaction
-problems.
+This repo builds upon IAMR (https://amrex-codes.github.io/IAMR/) code that solves the two-phase 
+incompressible Navier-Stokes equations using the level set (LS) method. This code aims at simulating the multiphase flow and fluid structure interaction (FSI)
+problems on CPUs and GPUs with/without subcycling.
 
-Please see documentation at https://amrex-codes.github.io/IAMR/
+## Features
+
+- LS method and reinitialization schemes
+
+## Tutorials
+
+- [Reversed Single Vortex (RSV)](./Tutorials/RSV/)
+
+<div align="center">
+<img src="./README_figures/RSV.jpeg" alt="Profiles of drop interface in the RSV problem at t/T=1 after one rotation. Black line: Analytical Solution; Red line: 64*64; Blue line: 128*128; Green line: 256*256">
+
+- Rayleigh-Taylor (RT) instability
+
--- Source/ls.py
+++ Source/ls.py
@@ -6,51 +6,52 @@ dx = 1  # Replace with your value
 dy = 1  # Replace with your value
 
 matrix = np.array([
-    [1, -dx, -dy,  dx*dy, dx*dx, dy*dy],
-    [1,   0, -dy,      0,     0, dy*dy],
-    [1,  dx, -dy, -dx*dy, dx*dx, dy*dy],
-    [1, -dx,   0,      0, dx*dx,     0],
-    [1,   0,   0,      0,     0,     0],
-    [1,  dx,   0,      0, dx*dx,     0],
-    [1, -dx,  dy, -dx*dy, dx*dx, dy*dy],
-    [1,   0,  dy,      0,     0, dy*dy],
-    [1,  dx,  dy,  dx*dy, dx*dx, dy*dy]
+    [1, -dx, -dy,  dx*dy, 0.5*dx*dx, 0.5*dy*dy],
+    [1,   0, -dy,      0,         0, 0.5*dy*dy],
+    [1,  dx, -dy, -dx*dy, 0.5*dx*dx, 0.5*dy*dy],
+    [1, -dx,   0,      0, 0.5*dx*dx,         0],
+    [1,   0,   0,      0,         0,         0],
+    [1,  dx,   0,      0, 0.5*dx*dx,         0],
+    [1, -dx,  dy, -dx*dy, 0.5*dx*dx, 0.5*dy*dy],
+    [1,   0,  dy,      0,         0, 0.5*dy*dy],
+    [1,  dx,  dy,  dx*dy, 0.5*dx*dx, 0.5*dy*dy]
 ])
 
-# print(matrix)
+print(matrix)
 
 dis = math.sqrt(2)/2
 rhs = [-dis, 0, -dis, 0, dis, 0, -dis, 0, -dis]
-# print(rhs)
+print(rhs)
 
 # Let us say wanna use the least square method to solver matrix * x = rhs, in whcih x is a 6 by 1 array.
 
 # Solve using least squares
 x, residuals, rank, s = np.linalg.lstsq(matrix, rhs, rcond=None)
 
-# print("\nSolution (x):")
-# print(x)
+print("\nSolution (x):")
+print(x)
 
-# # If you're interested in the residuals
-# print("\nResiduals:")
-# print(residuals)
+# If you're interested in the residuals
+print("\nResiduals:")
+print(residuals)
 
 # Enable pretty printing
 sp.init_printing(use_unicode=True, wrap_line=False)
 
 # Define symbolic variables
 dx, dy = sp.symbols('dx dy')
+half = sp.Rational(1,2)
 
 matrixsp = sp.Matrix([
-    [1, -dx, -dy,  dx*dy, dx*dx, dy*dy],
-    [1,   0, -dy,      0,     0, dy*dy],
-    [1,  dx, -dy, -dx*dy, dx*dx, dy*dy],
-    [1, -dx,   0,      0, dx*dx,     0],
-    [1,   0,   0,      0,     0,     0],
-    [1,  dx,   0,      0, dx*dx,     0],
-    [1, -dx,  dy, -dx*dy, dx*dx, dy*dy],
-    [1,   0,  dy,      0,     0, dy*dy],
-    [1,  dx,  dy,  dx*dy, dx*dx, dy*dy]
+    [1, -dx, -dy,  dx*dy, half*dx*dx, half*dy*dy],
+    [1,   0, -dy,      0,          0, half*dy*dy],
+    [1,  dx, -dy, -dx*dy, half*dx*dx, half*dy*dy],
+    [1, -dx,   0,      0, half*dx*dx,          0],
+    [1,   0,   0,      0,          0,          0],
+    [1,  dx,   0,      0, half*dx*dx,          0],
+    [1, -dx,  dy, -dx*dy, half*dx*dx, half*dy*dy],
+    [1,   0,  dy,      0,          0, half*dy*dy],
+    [1,  dx,  dy,  dx*dy, half*dx*dx, half*dy*dy]
 ])
 
 result = matrixsp.transpose() * matrixsp
@@ -73,7 +74,7 @@ result_num = result.subs({dx: dx_val, dy: dy_val})
 
 # Convert the SymPy matrix to a numpy array
 result_np = np.array(result_num.tolist(), dtype=float)
-# print(result_np)
+print(result_np)
 
 x_cal = np.linalg.inv(result_np) @ matrix.T @ rhs
-# print(x_cal)
+print(x_cal)
